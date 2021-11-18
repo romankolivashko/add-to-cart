770f2bf801563917e641405fd3a2be6d8cf8262a
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-17 19:56:49 -0800

    Added Remove Item option

--- src/Cart.js
+++ src/Cart.js
@@ -31,7 +31,7 @@ const Cart = () => {
                   <td>
                     <button className="btn btn-info ms-2" onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
                     <button className="btn btn-info ms-2" onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
-                    <button className="btn btn-danger ms-2">Remove Item</button>
+                    <button className="btn btn-danger ms-2" onClick={() => removeItem(item.id)}>Remove Item</button>
                   </td>
                 </tr>)
               })}
