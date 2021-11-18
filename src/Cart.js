2ceec364d952b932a093feeb6e20541b17cc7324
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-17 20:12:21 -0800

    added Buy Now option

--- src/Cart.js
+++ src/Cart.js
@@ -42,6 +42,12 @@ const Cart = () => {
             <div className="col-auto ms-auto">
               <h2>Total Price: $ { cartTotal }</h2>
             </div>
+            <div className="col-auto">
+                <button className="btn btn-danger m-2" onClick={() => emptyCart()}>
+                  Clear Cart
+                </button>
+                <button className="btn btn-primary m-2">Buy Now</button>
+            </div>
         </div>
     </section>
   );
