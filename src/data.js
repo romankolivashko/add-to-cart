0a9de7bbb72f0378c3d105c4af47411f19f3e9a4
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-17 20:28:56 -0800

    Added media

--- src/data.js
+++ src/data.js
@@ -1,27 +1,43 @@
-import img1 from './img/img1.png';
-import img2 from './img/img2.png';
-import img3 from './img/img3.png';
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
 
 const data = {
     productData:[
       {
         id: 1,
         img: img1,
-        title: 'Juicy Grapes',
+        title: 'Army Soldier',
         desc: '',
         price: 46,
       },
       {
         id: 2,
         img: img2,
-        title: 'Atlanta Peaches',
+        title: 'William Shakespeare',
         desc: '',
         price: 26,
       },
       {
         id: 3,
         img: img3,
-        title: 'Juicy Orange',
+        title: 'Business Man',
+        desc: '',
+        price: 27,
+      },
+      {
+        id: 4,
+        img: img4,
+        title: 'Mr. President',
+        desc: '',
+        price: 299,
+      },
+      {
+        id: 5,
+        img: img5,
+        title: 'We Can Do It!',
         desc: '',
         price: 36,
       }
